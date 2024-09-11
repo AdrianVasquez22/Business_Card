5534f3a2f39d28c473b513ff93364c3b37fa55f3
Adrian Vasquez <advasquez2@buffs.wtamu.edu>
2024-09-11 11:41:14 -0500

    Initial commit

---
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -0,0 +1,131 @@
+package com.example.businesscard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.businesscard.ui.theme.BusinessCardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BusinessCardTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color.Black
+                ) {
+                    CardImage(message = stringResource(R.string.name_text), from = stringResource(R.string.major_text))
+                }
+            }
+        }
+    }
+}
+
+
+
+@Composable
+fun CardImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(id = R.drawable.android_logo)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .size(200.dp)
+                .clip(RoundedCornerShape(16.dp))
+                .align(Alignment.TopCenter)
+                .height(50.dp)
+                .background(Color.Gray)
+        )
+        CardText(
+            message = message,
+            from = from,
+            modifier = modifier.fillMaxSize(),
+        )
+    }
+}
+
+@Composable
+fun CardText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .padding(start = 12.dp, end = 12.dp, top = 15.dp, bottom = 15.dp)
+            .fillMaxWidth() // Ensure the Column takes full width if needed
+    ) {
+        Spacer(modifier = Modifier.height(200.dp)) // Adds space at the top of the Column
+
+        Text(
+            text = message,
+            fontSize = 50.sp,
+            color = Color.Green
+        )
+        Text(
+            text = from,
+            fontSize = 35.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.width(450.dp),
+            color = Color.LightGray
+        )
+    }
+}
+
+@Composable
+fun SmallWidgetOne() {
+    val image = painterResource(id = R.drawable.instagram_logo)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .size(50.dp)
+                .clip(RoundedCornerShape(16.dp))
+                .align(Alignment.BottomCenter)
+                .height(15.dp)
+                .background(Color.Gray)
+        )
+    }
+}
+
+@Preview(
+        showBackground = true,
+        showSystemUi= true)
+    @Composable
+    fun BusinessCardPreview() {
+        BusinessCardTheme {
+            CardImage(message = stringResource(R.string.name_text), from = stringResource(R.string.major_text))
+            SmallWidgetOne()
+        }
+    }
+
+
+
+
+
